1429e9266264938483899cd1bf917d8db26d06be
afnf <af.not.found@gmail.com>
2015-12-14 21:30:21 +0900

    format deploy.sh

--- itamae/cookbooks/springboot-zdtd/files/var/zdtd/deploy.sh
+++ itamae/cookbooks/springboot-zdtd/files/var/zdtd/deploy.sh
@@ -5,23 +5,18 @@ set -eu
 ln -f /var/zdtd/zdtd-next.jar /var/zdtd/zdtd-8081.jar
 
 /etc/init.d/zdtd-8081 start
-
-echo "starting 8081, sleeping 60sec"
-sleep 60s;
+echo "starting 8081, sleeping 30sec..."
+sleep 30s;
 
 /etc/init.d/zdtd-8080 stop
-
-echo "stopping 8080, sleeping 20sec"
-sleep 20s;
+echo "stopping 8080, sleeping 10sec..."
+sleep 10s;
 
 ln -f /var/zdtd/zdtd-next.jar /var/zdtd/zdtd-8080.jar
-
 /etc/init.d/zdtd-8080 start
-
-echo "starting 8080, sleeping 60sec"
-sleep 60s;
+echo "starting 8080, sleeping 30sec..."
+sleep 30s;
 
 /etc/init.d/zdtd-8081 stop
-
 mv /var/zdtd/zdtd-next.jar /var/zdtd/zdtd-current.jar
-
+echo "finished."
